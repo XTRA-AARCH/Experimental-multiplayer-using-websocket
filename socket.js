74c6d717c62e11de23e7f32b30be6005e2689277
XTRA-AARCH <SEEKER2079@outlook.com>
2025-11-05 14:31:46 -0500

    Add socket.js for WebSocket connection

---
+++ socket.js
@@ -0,0 +1,5 @@
+// socket.js
+import { io } from "socket.io-client";
+
+// Your live Replit WebSocket server
+export const IDKWHATIAMDOINGHERE = io("wss://5e7a188b-a598-4ea4-943c-edddf081b41e-00-3xsd92yd0n8.spock.replit.dev");
