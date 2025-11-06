dba14142032bf5090093129cc5e530f1bf8b3664
XTRA-AARCH <SEEKER2079@outlook.com>
2025-11-06 13:43:48 -0500

    Refactor multiplayerSync.js to improve function exports

--- multiplayerSync.js
+++ multiplayerSync.js
@@ -1,13 +1,10 @@
 // multiplayerSync.js
-import { IDKWHATIAMDOINGHERE } from "./socket.js";
 
-// Local player state
 let otherPlayers = {};
 let syncedEnemies = {};
 let syncedBoss = {};
 
-// Send your player position every frame
-export function sendPlayerUpdate(player) {
+function sendPlayerUpdate(player) {
   IDKWHATIAMDOINGHERE.emit("playerUpdate", {
     id: IDKWHATIAMDOINGHERE.id,
     x: player.x,
@@ -17,13 +14,11 @@ export function sendPlayerUpdate(player) {
   });
 }
 
-// Receive other players
 IDKWHATIAMDOINGHERE.on("playerUpdate", data => {
   otherPlayers[data.id] = data;
 });
 
-// Send enemy positions
-export function sendEnemyState(enemies) {
+function sendEnemyState(enemies) {
   IDKWHATIAMDOINGHERE.emit("enemyState", {
     enemies: enemies.map(e => ({
       id: e.id,
@@ -35,46 +30,41 @@ export function sendEnemyState(enemies) {
   });
 }
 
-// Receive enemy positions
 IDKWHATIAMDOINGHERE.on("enemyState", data => {
   data.enemies.forEach(e => {
     syncedEnemies[e.id] = e;
   });
 });
 
-// Send enemy damage
-export function sendEnemyHit(enemyId, damage) {
+function sendEnemyHit(enemyId, damage) {
   IDKWHATIAMDOINGHERE.emit("enemyHit", { enemyId, damage });
 }
 
-// Receive enemy damage
 IDKWHATIAMDOINGHERE.on("enemyHit", data => {
   if (syncedEnemies[data.enemyId]) {
     syncedEnemies[data.enemyId].hp -= data.damage;
   }
 });
 
-// Send boss damage
-export function sendBossTrigger(bossId, damage) {
+function sendBossTrigger(bossId, damage) {
   IDKWHATIAMDOINGHERE.emit("bossTrigger", { bossId, damage });
 }
 
-// Receive boss damage
 IDKWHATIAMDOINGHERE.on("bossTrigger", data => {
   if (syncedBoss[data.bossId]) {
     syncedBoss[data.bossId].hp -= data.damage;
   }
 });
 
-// Export synced states
-export function getSyncedPlayers() {
-  return otherPlayers;
-}
+function getSyncedPlayers() { return otherPlayers; }
+function getSyncedEnemies() { return syncedEnemies; }
+function getSyncedBoss() { return syncedBoss; }
 
-export function getSyncedEnemies() {
-  return syncedEnemies;
-}
-
-export function getSyncedBoss() {
-  return syncedBoss;
-}
+// ✅ expose globally
+window.sendPlayerUpdate = sendPlayerUpdate;
+window.sendEnemyState = sendEnemyState;
+window.sendEnemyHit = sendEnemyHit;
+window.sendBossTrigger = sendBossTrigger;
+window.getSyncedPlayers = getSyncedPlayers;
+window.getSyncedEnemies = getSyncedEnemies;
+window.getSyncedBoss = getSyncedBoss;
